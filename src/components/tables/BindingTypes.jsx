85bf95eb71a900a47c5fd75fc2873ba22e8c0a8d
BohdaR <bohdanshushval@gmail.com>
2022-12-12 02:08:41 +0200

    implemented BindingTypes table

--- src/components/tables/BindingTypes.jsx
+++ src/components/tables/BindingTypes.jsx
@@ -1,9 +1,56 @@
-import React from 'react';
+import React, {useState} from 'react';
+import TableName from "../TableName";
+import BaseForm from "../forms/BaseForm";
+import FormTextInput from "../inputs/FormTextInput";
+import BindingTypesRow from "./rows/BindingTypesRow";
 
-const BindingTypes = () => {
+const BindingTypes = ({bindingTypes, setBindingTypes, setShowLoader, setErrors}) => {
+    const [name, setName] = useState('')
+
     return (
-        <div>
-
+        <div className="container">
+            <TableName
+                tableName="countries"
+                setRows={setBindingTypes}
+                setShowLoader={setShowLoader}
+                setErrors={setErrors}
+            />
+            <table>
+                <thead>
+                <tr>
+                    <th><h1>id</h1></th>
+                    <th><h1>name</h1></th>
+                    <th colSpan="2">
+                        <BaseForm
+                            urlTableName="countries"
+                            tableRows={bindingTypes}
+                            setTableRows={setBindingTypes}
+                            data={{name: name}}
+                        >
+                            <FormTextInput
+                                autoFocus
+                                label="Name"
+                                name="name"
+                                value={name}
+                                type="text"
+                                handleChange={setName}
+                            />
+                        </BaseForm>
+                    </th>
+                </tr>
+                </thead>
+                <tbody>
+                {bindingTypes.map(bindingType =>
+                    <BindingTypesRow
+                        key={bindingType.id}
+                        bindingType={bindingType}
+                        bindingTypes={bindingTypes}
+                        setErrors={setErrors}
+                        setShowLoader={setShowLoader}
+                    />
+                )}
+                </tbody>
+            </table>
         </div>
     );
 };
--- src/components/tables/rows/BindingTypesRow.jsx
+++ src/components/tables/rows/BindingTypesRow.jsx
@@ -1,10 +1,69 @@
-import React from 'react';
+import React, {Fragment, useState} from 'react';
+import {deleteRow, updateRow} from "../../../useAPI/useAPI";
 
-const BindingTypesRow = () => {
+const BindingTypesRow = ({bindingTypes, setErrors, setShowLoader, ...props}) => {
+    const [bindingType, setBindingType] = useState(props.bindingType)
+
+    const [name, setName] = useState(bindingType.name)
+    const [bindingTypeId, setBindingTypeId] = useState(bindingType.id)
+    const [showRow, setShowRow] = useState(true)
+
+    const data = {
+        name: name,
+    }
+
     return (
-        <div>
-            
-        </div>
+        <Fragment>
+            {showRow ?
+                <tr>
+                    <td>
+                        <input
+                            name="country_id"
+                            value={bindingTypeId}
+                            type="text"
+                            className="col-value"
+                            onChange={(e) => setBindingTypeId(e.target.value)}
+                        />
+                    </td>
+                    <td>
+                        <input
+                            name="name"
+                            value={name}
+                            type="text"
+                            className="col-value"
+                            onChange={(e) => setName(e.target.value)}
+                        />
+                    </td>
+                    <td>
+                        <input
+                            style={{cursor: "pointer"}}
+                            value="Save"
+                            type="button"
+                            className="col-value"
+                            onClick={() => updateRow(
+                                `${process.env.REACT_APP_API_HOST}/api/v1/binding_types/${bindingTypeId}`,
+                                data,
+                                setBindingType,
+                                setShowLoader,
+                                setErrors)}
+                        />
+                    </td>
+                    <td>
+                        <input
+                            style={{cursor: "pointer"}}
+                            value="Delete"
+                            type="button"
+                            className="col-value"
+                            onClick={() => deleteRow(
+                                `${process.env.REACT_APP_API_HOST}/api/v1/binding_types/${bindingTypeId}`,
+                                setShowRow,
+                                setShowLoader,
+                                setErrors
+                            )}
+                        />
+                    </td>
+                </tr> : null}
+        </Fragment>
     );
 };
 
